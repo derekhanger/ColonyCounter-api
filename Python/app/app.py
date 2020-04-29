013a22cfbb32889ff47eeb32680cd3644be04dc4
Hanger, Derek T.(S&T-Student) <dth5h8@mst.edu>
2020-04-29 02:22:20 -0500

    first call working

--- Python/app/app.py
+++ Python/app/app.py
@@ -4,7 +4,12 @@ from requests_toolbelt.multipart import decoder
 import requests
 import base64
 import io
+from PIL import Image
+from scipy import countingFunction
+import os
+import io
 import Image
+from array import array
 
 app = Flask(__name__)
 CORS(app)
@@ -16,12 +21,26 @@ def index():
 
 @app.route('/upload', methods = ['POST'])
 def upload():
-
+    
     img = request.files['imageFile']
+    with open('assets/file.temp', 'w') as f:
+        f.write(img)
 
+    def readimage(path):
+        with open(path, "rb") as f:
+            return bytearray(f.read())
+
+    bytez = readimage('assets/file.temp')
+
+    image = Image.open(io.BytesIO(bytez))
+    image.save("assets/picture.png")
 
     return 'uploaded file'
 
+
+@app.route('/getCount', methods = ['GET'])
+def getCount():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
